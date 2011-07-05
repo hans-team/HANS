122d50a78694f73702cbf23530c97af56132c476
Antonio Hernández <ahernandez@emergya.com>
2011-07-05 13:03:46 +0200

    Refactorize the action selector dialog.

--- hans/ActionSelectorDialog.py
+++ hans/ActionSelectorDialog.py
@@ -1,39 +1,35 @@
 #!/usr/bin/python
 
 import gtk
+import types
+import gettext
+from gettext import gettext as _
 
 from hans.helpers import get_builder
 from hans import InterfaceEntry
 
-import gettext
-from gettext import gettext as _
-
 gettext.textdomain('plauncher')
 
-class ActionSelectorDialog(gtk.Dialog):
+class ActionSelectorDialog(gtk.Window):
 
     __gtype_name__ = "ActionSelectorDialog"
 
-    def __init__(self, iface=None):
-        gtk.Dialog.__init__(self)
-        self._iface = iface
-        self._set_is_default = False
+    def __init__(self, device=None, execute_callback=None):
+        gtk.Window.__init__(self)
 
-        self._load_actions()
+    def main(self):
+        gtk.main()
 
-    def __new__(cls, iface=None):
+    def __new__(cls, device=None, execute_callback=None):
         """Special static method that's automatically called by Python when 
         constructing a new instance of this class.
         """
         builder = get_builder('ActionSelectorDialog')
         new_object = builder.get_object('ActionSelectorDialog')
-        new_object._treeviewActions = builder.get_object('treeviewActions')
-        new_object._lblDescription = builder.get_object('lblDescription')
-        new_object._btnExec = builder.get_object('btnExec')
-        new_object.finish_initializing(builder)
+        new_object.finish_initializing(builder, device, execute_callback)
         return new_object
 
-    def finish_initializing(self, builder):
+    def finish_initializing(self, builder, device, execute_callback):
         """Called when we're finished initializing.
 
         finish_initalizing should be called after parsing the ui definition
@@ -41,31 +37,50 @@ class ActionSelectorDialog(gtk.Dialog):
         finish initializing the start of the new ActionSelectorDialog
         instance.
         """
+
+        self.treeviewActions = builder.get_object('treeviewActions')
+        #self.lblDescription = builder.get_object('lblDescription')
+        self.btnExecute = builder.get_object('btnExecute')
+        self.checkDefaultAction = builder.get_object('checkDefaultAction')
+
         # Get a reference to the builder and set up the signals.
         self.builder = builder
+        self.device = device
+        self._selected_interface = None
         self.builder.connect_signals(self)
+        if type(execute_callback) == types.FunctionType:
+            self.btnExecute.connect('clicked', execute_callback, self)
 
-    def on_checkDefaultActionToggled(self, togglebutton):
-        self._set_is_default = togglebutton.get_active()
+        self._init_treeview()
+        self._load_interfaces()
+        self.show()
 
-    def on_btnExecClicked(self, button):
-        # TODO: Do something with the get_is_default_action value
+    def on_deleteEvent(self, widget, data=None):
+        gtk.main_quit()
+
+    def on_btnExecuteClicked(self, button):
+        #self.destroy()
         pass
 
     def on_btnCancelClicked(self, button):
+        self.destroy()
+
+    def on_checkDefaultActionToggled(self, togglebutton):
         pass
 
     def on_treeviewActionsRowActivated(self, treeview, path, user_param1):
-        # TODO: Do something with the get_is_default_action value
-        self.response(self.get_response_for_widget(self._btnExec))
+        pass
 
-    def get_is_default_action(self):
-        return self._set_is_default
+    def get_set_as_default(self):
+        return self.checkDefaultAction.get_active()
+
+    def get_selected_interface(self):
+        return self._selected_interface
 
     def get_selected_actions(self):
 
         action_list = []
-        treeselection = self._treeviewActions.get_selection()
+        treeselection = self.treeviewActions.get_selection()
         (model, paths) = treeselection.get_selected_rows()
 
         for path in paths:
@@ -75,14 +90,8 @@ class ActionSelectorDialog(gtk.Dialog):
 
         return action_list
 
-    def _load_actions(self):
+    def _init_treeview(self):
 
-        try:
-            a = self._treeviewActions
-        except Exception, e:
-            return
-
-        self._treeviewActions.connect('row-activated', self.on_treeviewActionsRowActivated)
         tvcolumn = gtk.TreeViewColumn('Actions')
 
         cell = gtk.CellRendererPixbuf()
@@ -94,20 +103,47 @@ class ActionSelectorDialog(gtk.Dialog):
         tvcolumn.pack_start(cell, True)
         tvcolumn.set_cell_data_func(cell, self._render_column_text)
 
-        self._treeviewActions.append_column(tvcolumn)
-        self._treeviewActions.set_search_column(1)
-
+        self.treeviewActions.append_column(tvcolumn)
+        self.treeviewActions.set_search_column(1)
 
         treestore = gtk.TreeStore(object)
+        self.treeviewActions.set_model(treestore)
+        self.treeviewActions.set_rubber_banding(True)
+        selection = self.treeviewActions.get_selection()
+        selection.set_mode(gtk.SELECTION_MULTIPLE)
+        #selection.set_select_function(self._selection_callback, None, True)
 
-        action_list = self._iface.get_actions()
+    def _selection_callback(self, selection, model, path, is_selected, user_data):
+        pass
+
+    def _load_interfaces(self):
+
+        box = self.builder.get_object('buttonboxInterfaces')
+
+        ifaces = self.device.get_interfaces()
+        for iface in ifaces:
+            #iface = iface.getInterfaceEntry()
+            button = gtk.Button(label=iface.getName())
+            button.connect('clicked', self._load_actions, iface)
+            box.add(button)
+
+        box.show_all()
+
+    def _load_actions(self, button, iface):
+
+        self._selected_interface = iface
+
+        treestore = self.treeviewActions.get_model()
+        treestore.clear()
+
+        action_list = iface.get_actions()
         for name in action_list:
             action = action_list[name]
             if action.getInteractive():
                 treestore.append(None, [action])
 
-        self._treeviewActions.set_model(treestore)
-        self._treeviewActions.get_selection().set_mode(gtk.SELECTION_MULTIPLE)
+        self.treeviewActions.set_model(treestore)
+        #self.treeviewActions.get_selection().set_mode(gtk.SELECTION_MULTIPLE)
 
     def _render_column_pixbuf(self, column, cell, model, iter):
         action = model.get_value(iter, 0)

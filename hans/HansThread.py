87d4aa93aa039991b07074d09f1a842638d1a992
Antonio Hernández <ahernandez@emergya.com>
2011-07-11 14:50:39 +0200

    Use of HansThread from HansCore, implement the main logic.

--- hans/HansThread.py
+++ hans/HansThread.py
@@ -3,15 +3,30 @@ import gtk
 import threading
 from . import (ActionSelectorSimple, ActionLauncher)
 from model import (DeviceClass, InterfaceEntry)
+from utils import notify
 
 class HansThread(threading.Thread):
+
     def __init__(self, device):
         threading.Thread.__init__(self)
         self.device = device
 
     def run(self):
-        dialog = ActionSelectorSimple.ActionSelectorSimple(self.device, self.on_actionExecuted)
-        dialog.main()
+
+        notify("HANS - New device connected", self.device.get_formated_name(), self.device.get_pixbuf())
+
+        defaults = self.device.get_defaults_entry()
+        action_list = defaults.get_actions()
+
+        if len(action_list) != 0:
+            interface_filename = defaults.get_interface()
+            interface_entry = InterfaceEntry.InterfaceEntry(interface_filename)
+            action_list = action_list.split(';')
+            self.launch_actions(interface_entry, action_list)
+
+        else:
+            dialog = ActionSelectorSimple.ActionSelectorSimple(self.device, self.on_actionExecuted)
+            dialog.main()
 
     def on_actionExecuted(self, button, dialog):
 
@@ -28,14 +43,13 @@ class HansThread(threading.Thread):
 
         if set_as_default:
             de = dialog.device.get_defaults_entry()
-            de.setInterface(selected_interface.get_name())
-            de.setActions(';'.join(selected_actions))
+            de.set_interface(selected_interface.get_name())
+            de.set_actions(';'.join(selected_actions))
             de.write()
 
         if isinstance(selected_interface, InterfaceEntry.InterfaceEntry):
             self.launch_actions(selected_interface, selected_actions)
 
     def launch_actions(self, interface_entry, action_list):
-
         launcher = ActionLauncher.ActionLauncher(interface_entry)
         launcher.execute(action_list)

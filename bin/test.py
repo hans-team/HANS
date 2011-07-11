c19ae48169bab0cc05afa22c474e66d2dc726222
Antonio Hernández <ahernandez@emergya.com>
2011-07-11 11:57:11 +0200

    Test hans as a new thread.

--- bin/test.py
+++ bin/test.py
@@ -9,6 +9,7 @@ import pynotify
 import os
 import re
 import gtk
+import threading
 
 # Add project root directory (enable symlink, and trunk execution).
 PROJECT_ROOT_DIRECTORY = os.path.abspath(
@@ -35,46 +36,56 @@ LOG_FORMAT = '%(asctime)s %(levelname)s - %(message)s'
 LOG_DATE_FORMAT = '%H:%M:%S'
 
 
-def execute_callback(button, dialog):
+class HansThread(threading.Thread):
+    def __init__(self, sysfspath):
+        self.sysfspath = sysfspath
+        threading.Thread.__init__(self)
 
-    selected_interface = dialog.get_selected_interface()
-    selected_actions = dialog.get_selected_actions()
-    set_as_default = dialog.get_set_as_default()
-    #dialog.destroy()
+    def run(self):
+        #try:
+            device = DeviceClass.DeviceClass(self.sysfspath)
 
-    print '--------------------'
-    print selected_interface
-    print selected_actions
-    print set_as_default
-    print '--------------------'
+            #dialog = ActionSelectorDialog.ActionSelectorDialog(device, execute_callback)
+            #dialog.main()
 
-    if set_as_default:
-        de = dialog.device.get_defaults_entry()
-        de.setInterface(selected_interface.get_name())
-        de.setActions(';'.join(selected_actions))
-        de.write()
+            dialog = ActionSelectorSimple.ActionSelectorSimple(device, self.on_actionExecuted)
+            dialog.main()
 
-    if isinstance(selected_interface, InterfaceEntry.InterfaceEntry):
-        launch_actions(selected_interface, selected_actions)
+        #except Exception, e:
+        #    print e
 
-def launch_actions(interface_entry, action_list):
+    def on_actionExecuted(self, button, dialog):
 
-    launcher = ActionLauncher.ActionLauncher(interface_entry)
-    launcher.execute(action_list)
+        selected_interface = dialog.get_selected_interface()
+        selected_actions = dialog.get_selected_actions()
+        set_as_default = dialog.get_set_as_default()
+        #dialog.destroy()
+
+        print '--------------------'
+        print selected_interface
+        print selected_actions
+        print set_as_default
+        print '--------------------'
+
+        if set_as_default:
+            de = dialog.device.get_defaults_entry()
+            de.setInterface(selected_interface.get_name())
+            de.setActions(';'.join(selected_actions))
+            de.write()
+
+        if isinstance(selected_interface, InterfaceEntry.InterfaceEntry):
+            self.launch_actions(selected_interface, selected_actions)
+
+    def launch_actions(self, interface_entry, action_list):
+
+        launcher = ActionLauncher.ActionLauncher(interface_entry)
+        launcher.execute(action_list)
+
 
 if __name__ == "__main__":
 
-    #try:
-        syspath = '/sys/devices/pci0000:00/0000:00:1d.7/usb1/1-5'
-        syspath = '/sys/devices/pci0000:00/0000:00:1d.7/usb1/1-5'
-        device = DeviceClass.DeviceClass(syspath)
+    sysfspath = '/sys/devices/pci0000:00/0000:00:1d.7/usb1/1-5'
 
-        #dialog = ActionSelectorDialog.ActionSelectorDialog(device, execute_callback)
-        #dialog.main()
-
-        dialog = ActionSelectorSimple.ActionSelectorSimple(device, execute_callback)
-        dialog.main()
-
-    #except Exception, e:
-    #    print e
+    t = HansThread(sysfspath)
+    t.start()
 

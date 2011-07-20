fc37032419643a41744a4bbf10b533531fc09595
David Amian <damian@emergya.com>
2011-07-20 13:35:17 +0200

    Modified setup.py to prepare install package

--- setup.py
+++ setup.py
@@ -1,89 +1,77 @@
 #!/usr/bin/env python
-# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
-### BEGIN LICENSE
-# This file is in the public domain
-### END LICENSE
+# -*- coding: utf-8 -*-
 
-###################### DO NOT TOUCH THIS (HEAD TO THE SECOND PART) ######################
+import sys, os, platform
+from distutils.core import setup
+from DistUtilsExtra.command import *
+import glob
 
-import os
-import sys
+# Get current Python version
+python_version = platform.python_version_tuple()
 
-try:
-    import DistUtilsExtra.auto
-except ImportError:
-    print >> sys.stderr, 'To build hans you need https://launchpad.net/python-distutils-extra'
-    sys.exit(1)
-assert DistUtilsExtra.auto.__version__ >= '2.18', 'needs DistUtilsExtra.auto >= 2.18'
+# Setup the default install prefix
+prefix = sys.prefix
 
-def update_data_path(prefix, oldvalue=None):
+# Check our python is version 2.6 or higher
+if python_version[0] >= 2 and python_version[1] >= 6:
+    ## Set file location prefix accordingly
+    prefix = '/usr/local'
 
-    try:
-        fin = file('hans/hansconfig.py', 'r')
-        fout = file(fin.name + '.new', 'w')
+# Get the install prefix if one is specified from the command line
+for arg in sys.argv:
+    if arg.startswith('--prefix='):
+        prefix = arg[9:]
+        prefix = os.path.expandvars(prefix)
 
-        for line in fin:            
-            fields = line.split(' = ') # Separate variable from value
-            if fields[0] == '__hans_data_directory__':
-                # update to prefix, store oldvalue
-                if not oldvalue:
-                    oldvalue = fields[1]
-                    line = "%s = '%s'\n" % (fields[0], prefix)
-                else: # restore oldvalue
-                    line = "%s = %s" % (fields[0], oldvalue)
-            fout.write(line)
+# Gen .in files with @PREFIX@ replaced
+#for filename in ['udev-discover']:
+#    infile = open(filename + '.in', 'r')
+#    data = infile.read().replace('@PREFIX@', prefix)
+#    infile.close()
+#
+#    outfile = open(filename, 'w')
+#    outfile.write(data)
+#    outfile.close()
 
-        fout.flush()
-        fout.close()
-        fin.close()
-        os.rename(fout.name, fin.name)
-    except (OSError, IOError), e:
-        print ("ERROR: Can't find hans/hansconfig.py")
-        sys.exit(1)
-    return oldvalue
+setup(
+        name='hans',
+        version='0.1',
+        description='Hardware Actions and Notification System.',
+        author='David Amian Valle',
+        author_email='damian@emergya.com',
+        url='https://launchpad.net/hans',
 
-
-def update_desktop_file(datadir):
-
-    try:
-        fin = file('hans.desktop.in', 'r')
-        fout = file(fin.name + '.new', 'w')
-
-        for line in fin:            
-            if 'Icon=' in line:
-                line = "Icon=%s\n" % (datadir + 'media/icon.png')
-            fout.write(line)
-        fout.flush()
-        fout.close()
-        fin.close()
-        os.rename(fout.name, fin.name)
-    except (OSError, IOError), e:
-        print ("ERROR: Can't find hans.desktop.in")
-        sys.exit(1)
-
-
-class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
-    def run(self):
-        previous_value = update_data_path(self.prefix + '/share/hans/')
-        update_desktop_file(self.prefix + '/share/hans/')
-        DistUtilsExtra.auto.install_auto.run(self)
-        update_data_path(self.prefix, previous_value)
-
-
+        classifiers=[
+            'Development Status :: 0.1 - Alpha',
+            'Environment :: Desktop Environment',
+            'License :: OSI Approved :: GNU General Public License (GPL)',
+            'Operating System :: POSIX',
+            'Programming Language :: Python',
+	        'Topic :: Utilities'
+        ],
         
-##################################################################################
-###################### YOU SHOULD MODIFY ONLY WHAT IS BELOW ######################
-##################################################################################
+        keywords = ['python', 'udev', 'gnome'],
 
-DistUtilsExtra.auto.setup(
-    name='hans',
-    version='0.1',
-    #license='GPL-3',
-    #author='Your Name',
-    #author_email='email@ubuntu.com',
-    #description='UI for managing …',
-    #long_description='Here a longer description',
-    #url='https://launchpad.net/hans',
-    cmdclass={'install': InstallAndUpdateDataDirectory}
-    )
+        packages = ['hans', 'hans.actions', 
+            'hans.model'], 
+        package_dir =  {'hans': 'hans', 
+            'hans.actions': 'hans/actions',
+            'hans.model': 'hans/model',
+            },
 
+        scripts = ['bin/hans.py'],
+        
+        data_files = [
+            ('bin', ['bin/hans']),
+            ('share/hans/db', glob.glob('data/db/*interface')),
+            ('share/hans/db/actions', glob.glob('data/db/actions/*action')),
+            ('share/hans/media', glob.glob('data/media/*')),
+            ('share/hans/ui', glob.glob('data/ui/*')),
+            ('/etc/udev/rules.d', ['udev-rules/99-hans-usb.rules']),
+        ],
+        cmdclass = { 
+            "build" : build_extra.build_extra,
+            "build_i18n" :  build_i18n.build_i18n,
+            "clean": clean_i18n.clean_i18n,
+        }
+)

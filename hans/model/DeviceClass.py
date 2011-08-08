3db2e2cb39214dc5a965efb1ad1990082b200e2e
Juanje Ojeda <jojeda@emergya.es>
2011-08-08 18:02:07 +0200

    Added headers and copyright notices

--- hans/model/DeviceClass.py
+++ hans/model/DeviceClass.py
@@ -1,3 +1,26 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+# Copyright (c) 2011 Junta de Andalucia
+#
+# Authors:
+#    Antonio Hernández <ahernandez at emergya.com>
+#    David Amian <damian at emergya.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+# USA
+
 import os.path
 import gudev
 import gettext
